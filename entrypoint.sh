7b4161d58b370ee818bfc74bcfaa752e7f160bfd
hvandang@outlook.com <hvandang@outlook.com>
2020-12-15 12:54:04 +0000

    revert change

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,7 +10,7 @@
 echo "Apply database migrations"
 python3 manage.py makemigrations
 python3 manage.py migrate
-python3 manage.py collectstatic --noinput
+python3 manage.py collectstatic
 
 # configure sseConfig
 sed -i -e "s|ta_url|$TA_URL|" sse/static/js/sse.js
