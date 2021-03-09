b88c68f6c120f3aab43ffe242d3fac04e2bbfe0c
hvandang@outlook.com <hvandang@outlook.com>
2021-03-09 11:07:25 +0000

    fix: get Django secret_key and allowed_host from environmental variables

--- SSEclient/settings.py
+++ SSEclient/settings.py
@@ -20,12 +20,13 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '5qea2o2*9wq5@42qy!on$j@cjm7j2mn)zby%25tt%6jeou!yeq'
+#SECRET_KEY = '5qea2o2*9wq5@42qy!on$j@cjm7j2mn)zby%25tt%6jeou!yeq'
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DJANGO_DEBUG')
 
-ALLOWED_HOSTS = '*' # os.getenv('ALLOWED_HOSTS').split(' ')
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split(' ')
 
 
 # Application definition
