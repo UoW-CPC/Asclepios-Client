423854757925140451b0f32d66078a3238525b6b
hvandang@outlook.com <hvandang@outlook.com>
2021-03-26 23:18:04 +0000

    feat: add auth_value and small_file environment variable

--- entrypoint.sh
+++ entrypoint.sh
@@ -37,6 +37,8 @@ sed -i -e "s|adata_len_ta_value|$ADATA_LEN_TA|" sse/static/js/sse.js
 sed -i -e "s|sgx_enable_value|$SGX_ENABLE|" sse/static/js/sse.js
 sed -i -e "s|cp_abe_url|$CP_ABE_URL|" sse/static/js/sse.js
 sed -i -e "s|debug_value|$DEBUG|" sse/static/js/sse.js
+sed -i -e "s|auth_value|$AUTH|" sse/static/js/sse.js
+sed -i -e "s|small_file|$SMALL_FILE|" sse/static/js/sse.js
 
 exec gunicorn SSEclient.wsgi:application \
     --name sse \
